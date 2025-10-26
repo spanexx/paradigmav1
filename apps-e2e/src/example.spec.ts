6a4a7a196ff2276bad8b6bfe6bafd5a7563e514d
spanexx <spanexxvictor@gmail.com>
2025-10-26 22:18:50 +0100

    fixed test issues

--- apps-e2e/src/example.spec.ts
+++ apps-e2e/src/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  // Expect hero heading to reflect console branding.
+  await expect(page.locator('h1')).toHaveText(/Asset Management Console/);
 });
