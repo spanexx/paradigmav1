f2c7769bf9dbdce39b215665e7da94b23be664bf
spanexx <spanexxvictor@gmail.com>
2025-10-26 22:18:50 +0100

    fixed test issues

--- apps/src/app/app.spec.ts
+++ apps/src/app/app.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
@@ -14,7 +15,7 @@ describe('App', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome admin'
+      'Asset Management Console'
     );
   });
 });
